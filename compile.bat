dcb592a1c822757a9066ac55dca0ca0f9d251bcf
Rogier Schouten <rogier.schouten@gmail.com>
2014-07-16 23:19:54 +0200

    Removed compile.bat since gulp build now includes all these functions

--- compile.bat
+++
@@ -1,6 +0,0 @@
-
-tsc --module commonjs --target es5 --declaration ./lib/index.ts ./test/test-index.ts ./examples/example.ts
-echo "typescript compiled."
-node ./dts.js
-echo "declarations bundled."
-typedoc --out "./doc" --name "timezonecomplete" --module commonjs --target es5 --excludeExternals ./lib/index.ts
